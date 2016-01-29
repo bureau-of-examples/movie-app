3ef0fdb7d97e5d6fdfe36510fc749f373bdf5bee
zhy2002 <hz141@uowmail.edu.au>
2016-01-29 22:56:31 +1100

    set up ng-mocks.

--- karma.conf.js
+++ karma.conf.js
@@ -15,7 +15,7 @@ module.exports = function(config) {
 
     // list of files / patterns to load in the browser
     files: [
-      'src/**/*.js', 'spec/**/*.js'
+      'bower_components/angular/angular.min.js', 'bower_components/angular-mocks/angular-mocks.js', 'src/**/*.js', 'spec/**/*.js'
     ],
 
 
--- readme.md
+++ readme.md
@@ -1,3 +1,6 @@
 ### Run karma with jasmine
 First do <code>npm install</code> and <code>npm install -g karma-cli</code>.
 Then start karma with <code>npm test</code>.
+
+* Demo data is retrieved from http://www.omdbapi.com.
+
