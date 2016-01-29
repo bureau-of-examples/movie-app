3a25f6616840dd558f366113a50b095460ded73e
zhy2002 <hz141@uowmail.edu.au>
2016-01-29 22:16:25 +1100

    setup karma with jasmine.

---
+++ readme.md
@@ -0,0 +1,3 @@
+### Run karma with jasmine
+First do <code>npm install</code> and <code>npm install -g karma-cli</code>.
+Then start karma with <code>npm test</code>.
---
+++ spec/calculator-spec.js
@@ -0,0 +1,11 @@
+describe('calculator', function(){
+   it('should add two numbers', function(){
+      expect(add(1,2)).toBe(3);
+   });
+});
+
+describe('my test', function(){
+    it('should return 1', function(){
+       expect(add(2,-1)).toBe(1);
+    });
+});
