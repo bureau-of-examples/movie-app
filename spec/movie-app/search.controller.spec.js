382a8b043ac304198de572c7b3d35a83bba49cc6
zhy2002 <hz141@uowmail.edu.au>
2016-02-05 22:25:09 +1100

    tested $timeout.

--- spec/movie-app/search.controller.spec.js
+++ spec/movie-app/search.controller.spec.js
@@ -1,22 +1,44 @@
 describe('Search controller', function () {
 
-    var $location = {}, $controller;
+    var $location = {}, $controller, $timeout;
 
     beforeEach(module('movieApp'));
-    beforeEach(inject(function (_$controller_, _$location_) {
+    beforeEach(inject(function (_$controller_, _$location_, _$timeout_) {
         $location = _$location_;
         $controller = _$controller_;
+        $timeout = _$timeout_;
     }));
 
+    function createSearchController(query) {
+        return $controller('SearchController', {$location: $location, $timeout: $timeout}, {query: query});
+    }
+
     it('should redirect to the query results page for non-empty query', function () {
-        var $this = $controller('SearchController', {$location: $location}, {query: 'star wars'}); //call controller
+        var $this = createSearchController('star wars'); //call controller
         $this.search();
         expect($location.url()).toBe('/results?q=star%20wars');
     });
 
     it('should not redirect to query results for empty query', function () {
-        var $this = $controller('SearchController', {$location: $location}, {query: ''}); //call controller
+        var $this = createSearchController(''); //call controller
         $this.search();
         expect($location.url()).toBe('');
+    });
+
+    it('should redirect after 1 second of keyboard inactivity', function(){
+        var $this = createSearchController('star wars');//call controller
+        $this.keyup();
+        $timeout.flush();
+        expect($location.url()).toBe('/results?q=star%20wars');
+        $timeout.verifyNoPendingTasks();
+    });
+
+    it('should cancel timeout in keyup', function(){
+        var $this = createSearchController('star wars');//call controller
+        spyOn($this, 'search');
+        $this.keyup();
+        $this.keyup();
+        $timeout.flush();
+        expect($this.search).toHaveBeenCalledTimes(1);
     });
 });
--- src/movie-app/search.controller.js
+++ src/movie-app/search.controller.js
@@ -1,12 +1,24 @@
 (function(movieAppModule){
 
-    movieAppModule.controller('SearchController', function ($location) {
+    movieAppModule.controller('SearchController', function ($location, $timeout) {
         var vm = this;
         vm.search = function () {
             if (vm.query) {
                 console.log('Searching key words: ' + vm.query);
                 $location.path('/results').search('q', vm.query);
             }
+        };
+
+        var previousTimeoutHandle = null;
+
+        vm.keyup = function(){
+            if(previousTimeoutHandle) {
+                $timeout.cancel(previousTimeoutHandle);
+            }
+            previousTimeoutHandle = $timeout(function(){
+                vm.search();
+                previousTimeoutHandle = null;
+            }, 1000);
         };
     });
 
