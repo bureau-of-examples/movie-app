730ce3d7a663c37b536a086cc375af5aba2a0bc2
zhy2002 <hz141@uowmail.edu.au>
2016-02-06 16:37:14 +1100

    Enabled expansion of search results.

--- src/movie-app/results.controller.js
+++ src/movie-app/results.controller.js
@@ -1,6 +1,6 @@
 (function (movieAppModule) {
 
-    movieAppModule.controller('ResultsController', ['$location', 'omdbApi', function ($location, omdbApi) {
+    movieAppModule.controller('ResultsController', ['$location', 'omdbApi',function ($location, omdbApi) {
         var vm = this;
         var query = $location.search().q;
         omdbApi.search(query).then(
@@ -12,6 +12,24 @@
                 vm.error = err;
             }
         );
+
+        vm.expand = function(index, id) {
+            var result = vm.results[index];
+            if(result.isOpen) {
+                result.isOpen = false;
+            } else {
+                if(result.data) {
+                    result.isOpen = true;
+                } else {
+                    omdbApi.find(id)
+                        .then(function(data){
+                            result.data = data;
+                            result.isOpen = true;
+                           // $scope.$digest();
+                        });
+                }
+            }
+        }
     }]);
 
 }(angular.module('movieApp')));
