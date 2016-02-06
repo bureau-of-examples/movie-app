ba5f7cbc711215b48f75387eb8e3a337c8704b60
zhy2002 <hz141@uowmail.edu.au>
2016-02-06 16:21:00 +1100

    Tested root scope.

--- spec/movie-app/movie-result.directive.spec.js
+++ spec/movie-app/movie-result.directive.spec.js
@@ -26,4 +26,57 @@ describe('Movie Result Directive', function () {
         var imgAlt = element.find("img").attr('alt');
         expect(imgAlt).toBe('Star Wars: Episode III - Revenge of the Sith');
     });
+
+});
+
+describe('$rootScope', function() {
+
+    beforeEach(module('movieApp'));
+
+    var $rootScope;
+    beforeEach(inject(function(_$rootScope_) {
+        $rootScope = _$rootScope_;
+    }));
+
+    it('should count descendant scopes', function(){
+        var childScopeCount = $rootScope.$countChildScopes();
+        expect(childScopeCount).toBe(0);
+
+        var childScope = $rootScope.$new();
+        childScopeCount = $rootScope.$countChildScopes();
+        expect(childScopeCount).toBe(1);
+
+        childScope.$new();
+        childScopeCount = $rootScope.$countChildScopes();
+        expect(childScopeCount).toBe(2);
+    });
+
+    it('should reset $rootScope', function(){
+        expect($rootScope.$countChildScopes()).toBe(0);
+    });
+
+    it('should call listener', function(){
+        $rootScope.selectedIndex = 4;
+        var watcherCalled = false;
+        $rootScope.$watch('selectedIndex', function(){
+            watcherCalled = true;
+        });
+        $rootScope.selectedIndex = 0;
+        $rootScope.$digest();
+        expect(watcherCalled).toBe(true);
+    });
+
+
+    it('should count child watcher', function(){
+        expect($rootScope.$countWatchers()).toBe(0);
+
+        var childScope = $rootScope.$new();
+        childScope.$watch('model', function(){});
+
+        expect($rootScope.$countWatchers()).toBe(1);
+    });
+
+
+
+
 });
